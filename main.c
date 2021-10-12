d47ce079db11c947caa07bfa7dc94a19ad4dd14f
ELORCHI <elorchie@gmail.com>
2021-10-12 18:52:37 +0100

    what is time

--- main.c
+++ main.c
@@ -6,13 +6,17 @@
 /*   By: eel-orch <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/11 15:39:56 by eel-orch          #+#    #+#             */
-/*   Updated: 2021/10/12 11:56:11 by eel-orch         ###   ########.fr       */
+/*   Updated: 2021/10/12 16:17:46 by eel-orch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "philo.h"
+#include "srcs/srcs.h"
 
 int main(int argc, char **argv)
 {
+	t_inpho		*info;
+
+	inpho = NULL;
 	check_args(argc, argv);
+	get_inpho(argc, argv, info);
 }
