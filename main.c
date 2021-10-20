de3e6c843978a84ba8558a6b46d92d4b138255e8
ELORCHI <elorchie@gmail.com>
2021-10-20 16:53:39 +0100

    simulation

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: eel-orch <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/11 15:39:56 by eel-orch          #+#    #+#             */
-/*   Updated: 2021/10/20 15:18:18 by eel-orch         ###   ########.fr       */
+/*   Updated: 2021/10/20 16:49:44 by eel-orch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,12 @@
 int main(int argc, char **argv)
 {
 	t_inpho		*info;
-	t_philo		*philo;
+	t_philo		*philos;
 
 	inpho = NULL;
 	philo = NULL;
 	if (check_args(argc, argv) == 1)
 		return (1);
 	get_inpho(argc, argv, info);
-	init_philos
+	philos = init_philos(info);
 }
--- srcs/init_philos.c
+++ srcs/init_philos.c
@@ -6,13 +6,13 @@
 /*   By: eel-orch <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/20 15:18:44 by eel-orch          #+#    #+#             */
-/*   Updated: 2021/10/20 16:46:03 by eel-orch         ###   ########.fr       */
+/*   Updated: 2021/10/20 16:53:18 by eel-orch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "srcs.h"
 
-t_philo *init_philos(t_info *info)
+t_philo	*init_philos(t_info *info)
 {
 	t_philo *philo;
 	int		i;
@@ -24,7 +24,7 @@ t_philo *init_philos(t_info *info)
 		philo[i].id = i + 1;
 		philo[i].meals_nb = 0;
 		philo[i].left_fork = i;
-		philo[i].right_fork = i % info->philos_nb;
+		philo[i].right_fork = (i + 1) % info->philos_nb;
 		i++;
 	}
 	philo[i] = NULL:
