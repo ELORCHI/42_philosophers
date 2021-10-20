1ad3aa11e6aa18be05cd2ff00eb003f27843782a
ELORCHI <elorchie@gmail.com>
2021-10-20 16:47:26 +0100

    where is my cheese

--- srcs/init_philos.c
+++ srcs/init_philos.c
@@ -1,26 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   init_philos.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: eel-orch <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/10/11 15:39:56 by eel-orch          #+#    #+#             */
-/*   Updated: 2021/10/20 15:18:18 by eel-orch         ###   ########.fr       */
+/*   Created: 2021/10/20 15:18:44 by eel-orch          #+#    #+#             */
+/*   Updated: 2021/10/20 16:46:03 by eel-orch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "srcs/srcs.h"
+#include "srcs.h"
 
-int main(int argc, char **argv)
+t_philo *init_philos(t_info *info)
 {
-	t_inpho		*info;
-	t_philo		*philo;
+	t_philo *philo;
+	int		i;
 
-	inpho = NULL;
-	philo = NULL;
-	if (check_args(argc, argv) == 1)
-		return (1);
-	get_inpho(argc, argv, info);
-	init_philos
+	i = 0;
+	philo = (t_philo *)malloc(sizeof(t_philo) * info->philos_nb + 1);
+	while (i < info->philos)
+	{
+		philo[i].id = i + 1;
+		philo[i].meals_nb = 0;
+		philo[i].left_fork = i;
+		philo[i].right_fork = i % info->philos_nb;
+		i++;
+	}
+	philo[i] = NULL:
 }
