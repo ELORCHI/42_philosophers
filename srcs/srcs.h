834dee17e8754c6bb0d7d3669ecbf9fcaab8f790
ELORCHI <elorchie@gmail.com>
2021-10-26 22:58:44 +0100

    restart

--- srcs/srcs.h
+++ srcs/srcs.h
@@ -30,25 +30,6 @@ typedef struct t_info
 	int	must_eat_nb;
 }				t_info;
 
-typedef struct s_sm
-{
-	pthread_mutex_t	*mutexs;
-	pthread_mutex_t	p;
-	pthread_mutex_t	is_eating;
-	uint64_t		start;
-}				t_sm;
-
-typedef struct s_philo
-{
-	int				id;
-	int				meals_nb;
-	int				left_fork;
-	int				right_fork;
-	pthread_t		*thread;
-	t_sm			*sm;	
-	uint64_t		last_meal_time;
-	t_info			*info;
-}				t_philo;
 
 int			ft_strcmp(const char *first, const char *second);
 int			check_args(int nb_args, char **args);
@@ -59,14 +40,4 @@ int			ft_isdigit(int c);
 void		print_error(char *error_msg);
 char		*ft_itoa(int n);
 t_info		*get_info(int argc, char **args);
-t_philo		*init_philos(t_info *info);
-void		*first_things_first(void *data);
-int			start_the_race(t_info *info, t_philo *philo);
-void		ft_sleep(int s);
-void		eat(t_philo *philo);
-void		display(t_philo *philo, char *msg);
-uint64_t	get_time(void);
-void		timer(int t);
-int			check_nb_meals(t_philo *philo);
-void		angel_of_death(t_philo *philo, t_info *info);
 #endif
